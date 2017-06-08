44cf191893a4143ed6b4256bfa3e218f68595fd6
amcamd <andrew.chapman@amd.com>
2017-06-08 18:09:37 -0500

    hipblas[Set,Get]PointerMode tests
    [ROCm/hipBLAS commit: 5a93c806b4658968c4528b531fb51fafd1e92893]

---
+++ clients/gtest/hipblas_gtest_main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************
+ * Copyright 2016 Advanced Micro Devices, Inc.
+ *
+ * ************************************************************************ */
+
+#include <gtest/gtest.h>
+#include <stdexcept>
+// #include "utility.h"
+
+/* =====================================================================
+      Main function:
+=================================================================== */
+
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
---
+++ clients/gtest/set_get_pointer_mode_gtest.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************
+ * Copyright 2016 Advanced Micro Devices, Inc.
+ *
+ * ************************************************************************ */
+
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include "hipblas.h"
+//#include "utility.h"
+
+using namespace std;
+
+/* =====================================================================
+README: This file contains testers to verify the correctness of
+        BLAS routines with google test
+
+        It is supposed to be played/used by advance / expert users
+        Normal users only need to get the library routines without testers
+     =================================================================== */
+
+/* =====================================================================
+     BLAS set-get_pointer_mode:
+=================================================================== */
+
+TEST(hipblas_set_pointer,hipblas_get_pointer)
+{
+    hipblasStatus_t status    = HIPBLAS_STATUS_SUCCESS;
+    hipblasPointerMode_t mode = HIPBLAS_POINTER_MODE_DEVICE;
+
+    hipblasHandle_t handle;
+    hipblasCreate(&handle);
+
+    status = hipblasSetPointerMode( handle, HIPBLAS_POINTER_MODE_DEVICE);
+    EXPECT_EQ(status, HIPBLAS_STATUS_SUCCESS);
+
+    status = hipblasGetPointerMode( handle, &mode);
+    EXPECT_EQ(status, HIPBLAS_STATUS_SUCCESS);
+
+    EXPECT_EQ(HIPBLAS_POINTER_MODE_DEVICE, mode);
+
+    status = hipblasSetPointerMode( handle, HIPBLAS_POINTER_MODE_HOST);
+    EXPECT_EQ(status, HIPBLAS_STATUS_SUCCESS);
+
+    status = hipblasGetPointerMode( handle, &mode);
+    EXPECT_EQ(status, HIPBLAS_STATUS_SUCCESS);
+
+    EXPECT_EQ(HIPBLAS_POINTER_MODE_HOST, mode);
+
+    hipblasDestroy(handle);
+}
