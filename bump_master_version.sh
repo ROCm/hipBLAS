fd403f1932a9b13bff8e6740b7e754a9a70aad3f
amcamd <andrew.chapman@amd.com>
2018-05-14 16:27:45 -0500

    add scripts to bump version
    [ROCm/hipBLAS commit: 26d885ab30260e36b8399b59c0002760904b7319]

---
+++ bump_master_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script needs to be edited to bump old develop version to new master version for new release.
+# - run this script in develop branch 
+# - after running this script merge develop into master
+# - after running this script and merging develop into master, run bump_develop_version.sh in master and
+#   merge master into develop
+
+OLD_HIPBLAS_VERSION="11.1.0"
+NEW_HIPBLAS_VERSION="10.3.0"
+
+sed -i "s/${OLD_HIPBLAS_VERSION}/${NEW_HIPBLAS_VERSION}/g" CMakeLists.txt
