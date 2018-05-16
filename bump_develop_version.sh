aaab05db823fd4ebda2da728871e70407ffe240b
amcamd <andrew.chapman@amd.com>
2018-05-16 17:27:18 -0500

    version for master branch release

--- bump_develop_version.sh
+++ bump_develop_version.sh
@@ -5,7 +5,7 @@
 # - run this script in master branch 
 # - after running this script merge master into develop 
 
-OLD_HIPBLAS_VERSION="10.3.0"
-NEW_HIPBLAS_VERSION="11.3.0"
+OLD_HIPBLAS_VERSION="10.3.1"
+NEW_HIPBLAS_VERSION="11.3.1"
 
 sed -i "s/${OLD_HIPBLAS_VERSION}/${NEW_HIPBLAS_VERSION}/g" CMakeLists.txt
