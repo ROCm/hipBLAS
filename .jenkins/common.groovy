73b7a686950fdb92382f61cc7e88491407fea619
Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>
2020-02-12 15:53:55 -0700

    Remove extended test file running duplicate tests (#157)
    [ROCm/hipBLAS commit: b83c391658f667e8d824c0fc7782737c1738f44c]

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -30,47 +30,21 @@ def runTestCommand (platform, project)
 
     if(platform.jenkinsLabel.contains('centos'))
     {
-        if(project.buildName == 'Extended')
-        {
-            echo "Running extended tests..."
-            command = """#!/usr/bin/env bash
-                    set -x
-                    cd ${project.paths.project_build_prefix}/build/release/clients/staging
-                    LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG sudo ./hipblas-test --gtest_output=xml --gtest_color=yes
-                    """
-        }
-        else
-        {
-            echo "Running precheckin tests..."
-            command = """#!/usr/bin/env bash
-                    set -x
-                    cd ${project.paths.project_build_prefix}/build/release/clients/staging
-                    LD_LIBRARY_PATH=/opt/rocm/hcc/lib sudo ./example-sscal
-                    LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG sudo ./hipblas-test --gtest_output=xml --gtest_color=yes
-                    """
-        }
+        command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}/build/release/clients/staging
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib sudo ./example-sscal
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG sudo ./hipblas-test --gtest_output=xml --gtest_color=yes
+                """
     }
     else
     {
-        if(project.buildName == 'Extended')
-        {
-            echo "Running extended tests..."
-            command = """#!/usr/bin/env bash
-                    set -x
-                    cd ${project.paths.project_build_prefix}/build/release/clients/staging
-                    LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./hipblas-test --gtest_output=xml --gtest_color=yes
-                    """
-        }
-        else
-        {
-            echo "Running precheckin tests..."
-            command = """#!/usr/bin/env bash
-                    set -x
-                    cd ${project.paths.project_build_prefix}/build/release/clients/staging
-                    LD_LIBRARY_PATH=/opt/rocm/hcc/lib ./example-sscal
-                    LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./hipblas-test --gtest_output=xml --gtest_color=yes
-                    """
-        }
+        command = """#!/usr/bin/env bash
+                set -x
+                cd ${project.paths.project_build_prefix}/build/release/clients/staging
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib ./example-sscal
+                LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./hipblas-test --gtest_output=xml --gtest_color=yes
+                """
     }
 
     platform.runCommand(this, command)
