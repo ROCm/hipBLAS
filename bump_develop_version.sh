4a8ad1587216ee8e9e25c630f5fe715ce80a222a
amcamd <andrew.chapman@amd.com>
2020-03-04 11:21:39 -0600

    version for master branch release
    [ROCm/hipBLAS commit: ad110695fd36d0f708e5ad39d940f6727f32f662]

--- bump_develop_version.sh
+++ bump_develop_version.sh
@@ -5,7 +5,7 @@
 # - run this script in master branch
 # - after running this script merge master into develop
 
-OLD_HIPBLAS_VERSION="0.22.0"
-NEW_HIPBLAS_VERSION="0.23.0"
+OLD_HIPBLAS_VERSION="0.24.0"
+NEW_HIPBLAS_VERSION="0.25.0"
 
 sed -i "s/${OLD_HIPBLAS_VERSION}/${NEW_HIPBLAS_VERSION}/g" CMakeLists.txt
