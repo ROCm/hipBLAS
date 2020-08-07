9192913c88e842571c0f74d498289b85190a618f
daineAMD <daine.mcniven@amd.com>
2020-08-07 18:22:17 -0500

    I guess cublas docs are wrong?

--- library/include/hipblas.h
+++ library/include/hipblas.h
@@ -7091,19 +7091,19 @@ HIPBLAS_EXPORT hipblasStatus_t hipblasTrsmStridedBatchedEx(hipblasHandle_t    ha
                                                            int                stride_invA,
                                                            hipblasDatatype_t  compute_type);
 
-HIPBLAS_EXPORT hipblasStatus_t hipblasCsyrkEx(hipblasHandle_t    handle,
-                                              hipblasFillMode_t  uplo,
-                                              hipblasOperation_t trans,
-                                              int                n,
-                                              int                k,
-                                              const float*       alpha,
-                                              const void*        A,
-                                              hipblasDatatype_t  Atype,
-                                              int                lda,
-                                              const float*       beta,
-                                              hipblasComplex*    C,
-                                              hipblasDatatype_t  Ctype,
-                                              int                ldc);
+HIPBLAS_EXPORT hipblasStatus_t hipblasCsyrkEx(hipblasHandle_t       handle,
+                                              hipblasFillMode_t     uplo,
+                                              hipblasOperation_t    trans,
+                                              int                   n,
+                                              int                   k,
+                                              const hipblasComplex* alpha,
+                                              const void*           A,
+                                              hipblasDatatype_t     Atype,
+                                              int                   lda,
+                                              const hipblasComplex* beta,
+                                              hipblasComplex*       C,
+                                              hipblasDatatype_t     Ctype,
+                                              int                   ldc);
 
 #ifdef __cplusplus
 }
