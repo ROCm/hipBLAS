61a9325d0e534c4a4ec05e075907707eb3c73c37
daineAMD <51674140+daineAMD@users.noreply.github.com>
2020-09-09 09:42:07 -0600

    Trsm fix (#250)

--- clients/gtest/hipblas_gtest_main.cpp
+++ clients/gtest/hipblas_gtest_main.cpp
@@ -13,6 +13,8 @@
 
 int main(int argc, char** argv)
 {
+    // Allocating 12MB
+    putenv("ROCBLAS_DEVICE_MEMORY_SIZE=12582912");
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
