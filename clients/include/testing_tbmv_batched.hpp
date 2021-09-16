6556085630dddc213ecafb5d40e406683432e094
daineAMD <daine.mcniven@amd.com>
2021-09-16 13:57:23 -0600

    tbmv incx changes.

--- clients/include/testing_tbmv_batched.hpp
+++ clients/include/testing_tbmv_batched.hpp
@@ -26,28 +26,30 @@ hipblasStatus_t testing_tbmv_batched(const Arguments& argus)
     int lda  = argus.lda;
     int incx = argus.incx;
 
-    size_t A_size = size_t(lda) * M;
-
-    int batch_count = argus.batch_count;
+    int    abs_incx    = incx >= 0 ? incx : -incx;
+    size_t A_size      = size_t(lda) * M;
+    int    batch_count = argus.batch_count;
 
     hipblasFillMode_t  uplo   = char2hipblas_fill(argus.uplo_option);
     hipblasOperation_t transA = char2hipblas_operation(argus.transA_option);
     hipblasDiagType_t  diag   = char2hipblas_diagonal(argus.diag_option);
     hipblasStatus_t    status = HIPBLAS_STATUS_SUCCESS;
 
+    hipblasLocalHandle handle(argus);
+
     // argument sanity check, quick return if input parameters are invalid before allocating invalid
     // memory
-    if(M < 0 || K < 0 || lda < M || incx == 0 || batch_count < 0)
+    bool invalid_size = M < 0 || K < 0 || lda < K + 1 || !incx || batch_count < 0;
+    if(invalid_size || !M || !batch_count)
     {
-        return HIPBLAS_STATUS_INVALID_VALUE;
-    }
-    else if(batch_count == 0)
-    {
-        return HIPBLAS_STATUS_SUCCESS;
+        hipblasStatus_t actual = hipblasTbmvBatchedFn(
+            handle, uplo, transA, diag, M, K, nullptr, lda, nullptr, incx, batch_count);
+        EXPECT_HIPBLAS_STATUS(
+            actual, (invalid_size ? HIPBLAS_STATUS_INVALID_VALUE : HIPBLAS_STATUS_SUCCESS));
+        return actual;
     }
 
-    double             gpu_time_used, hipblas_error;
-    hipblasLocalHandle handle(argus);
+    double gpu_time_used, hipblas_error;
 
     // arrays of pointers-to-host on host
     host_batch_vector<T> hA(A_size, 1, batch_count);
@@ -101,11 +103,11 @@ hipblasStatus_t testing_tbmv_batched(const Arguments& argus)
         // unit check and norm check can not be interchanged their order
         if(argus.unit_check)
         {
-            unit_check_general<T>(1, M, batch_count, incx, hx_cpu, hx_res);
+            unit_check_general<T>(1, M, batch_count, abs_incx, hx_cpu, hx_res);
         }
         if(argus.norm_check)
         {
-            hipblas_error = norm_check_general<T>('F', 1, M, incx, hx_cpu, hx_res, batch_count);
+            hipblas_error = norm_check_general<T>('F', 1, M, abs_incx, hx_cpu, hx_res, batch_count);
         }
     }
 
