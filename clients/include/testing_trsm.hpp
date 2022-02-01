bd3031d03d9bbe00ef4574a21f29f2878db955f4
daineAMD <daine.mcniven@amd.com>
2022-02-01 16:41:47 -0700

    cleanup.

--- clients/include/testing_trsm.hpp
+++ clients/include/testing_trsm.hpp
@@ -58,12 +58,8 @@ hipblasStatus_t testing_trsm(const Arguments& argus)
     hipblasLocalHandle handle(argus);
 
     // Initial hA on CPU
-    // srand(1);
-    // hipblas_init_symmetric<T>(hA, K, lda);
     hipblas_init_matrix(hA, argus, K, K, lda, 0, 1, hipblas_client_never_set_nan, true);
-    hipblas_init_matrix(
-        hB_host, argus, M, N, ldb, 0, 1, hipblas_client_never_set_nan); //, false, true);
-    // hipblas_init<T>(hB_host, M, N, ldb);
+    hipblas_init_matrix(hB_host, argus, M, N, ldb, 0, 1, hipblas_client_never_set_nan, false, true);
 
     // pad untouched area into zero
     for(int i = K; i < lda; i++)
