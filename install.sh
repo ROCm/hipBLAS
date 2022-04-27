136bc7add3e9cace840755c88a3b641856ffdee8
daineAMD <daine.mcniven@amd.com>
2022-04-27 14:48:00 -0600

    Adding a line.

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
 # CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
 # ########################################################################
 
 /bin/ln -fs ../../.githooks/pre-commit "$(dirname "$0")/.git/hooks/"
