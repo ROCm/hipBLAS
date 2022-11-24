5d1b3e5d242409bdd40f0d02ab77b0c2ebde73b0
Daine McNiven <51674140+daineAMD@users.noreply.github.com>
2022-11-24 10:54:44 -0700

    Increase gemm_ex tolerance for gfx11 (#549)
    Co-authored-by: amcamd <andrew.chapman@amd.com>
    
    [ROCm/hipBLAS commit: a89cfbed1381af078a2836345f6fd26cecd6ca6d]

--- clients/include/testing_gemm_strided_batched_ex.hpp
+++ clients/include/testing_gemm_strided_batched_ex.hpp
@@ -244,10 +244,23 @@ inline hipblasStatus_t testing_gemm_strided_batched_ex_template(const Arguments&
                                     ldc);
         }
 
-        if(arg.unit_check)
+        if(unit_check)
         {
-            unit_check_general<Tc>(M, N, batch_count, ldc, stride_C, hC_gold, hC_host);
-            unit_check_general<Tc>(M, N, batch_count, ldc, stride_C, hC_gold, hC_device);
+            // check for float16/bfloat16 input
+            if((getArchMajor() == 11)
+               && ((std::is_same<Tex, float>{} && std::is_same<Ta, hipblasBfloat16>{})
+                   || (std::is_same<Tex, float>{} && std::is_same<Ta, hipblasHalf>{})
+                   || (std::is_same<Tex, hipblasHalf>{} && std::is_same<Ta, hipblasHalf>{})))
+            {
+                const double tol = K * sum_error_tolerance_for_gfx11<Tex, Ta, Tc>;
+                near_check_general<Tc>(M, N, batch_count, ldc, stride_C, hC_gold, hC_host, tol);
+                near_check_general<Tc>(M, N, batch_count, ldc, stride_C, hC_gold, hC_device, tol);
+            }
+            else
+            {
+                unit_check_general<Tc>(M, N, batch_count, ldc, stride_C, hC_gold, hC_host);
+                unit_check_general<Tc>(M, N, batch_count, ldc, stride_C, hC_gold, hC_device);
+            }
         }
         if(arg.norm_check)
         {
