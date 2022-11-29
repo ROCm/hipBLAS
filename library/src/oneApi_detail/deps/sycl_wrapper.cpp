ea6fb706ff4e3e4c1c8cbed5c08790649b9bb432
Tu, Peng <peng.tu@intel.com>
2022-11-29 11:30:50 -0800

    Use DPCPP include directory from find_program, update level_zero.hpp path to the new ext directory

--- library/src/oneApi_detail/deps/sycl_wrapper.cpp
+++ library/src/oneApi_detail/deps/sycl_wrapper.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-#include "include/ze_api.h"
-#include "CL/sycl/backend/level_zero.hpp"
-#include "CL/sycl.hpp"
-#include "oneapi/mkl.hpp"
+#include <include/ze_api.h>
+#include <sycl.hpp>
+#include <ext/oneapi/backend/level_zero.hpp>
+#include <oneapi/mkl.hpp>
 
-#include <sycl_w.h>
+#include "sycl_w.h"
 
 void* get_current_context() {
 
