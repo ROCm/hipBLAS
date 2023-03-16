c76c78eb48cd5e42990e59f570592159a300e0ca
daineAMD <daine.mcniven@amd.com>
2023-03-16 16:56:56 -0600

    Removing another int8x4 function.

--- clients/include/utility.h
+++ clients/include/utility.h
@@ -433,38 +433,6 @@ inline hipblasBfloat16 random_hpl_generator()
 /* ============================================================================================ */
 
 /* ============================================================================================ */
-/*! \brief Packs strided_batched matricies into groups of 4 in N */
-template <typename T>
-void hipblas_packInt8(
-    std::vector<T>& A, size_t M, size_t N, size_t lda, size_t batch_count = 1, size_t stride_a = 0)
-{
-    if(N % 4 != 0)
-        std::cerr << "ERROR: dimension must be a multiple of 4 in order to pack" << std::endl;
-
-    std::vector<T> temp(A);
-    for(size_t b = 0; b < batch_count; b++)
-        for(size_t colBase = 0; colBase < N; colBase += 4)
-            for(size_t row = 0; row < lda; row++)
-                for(size_t colOffset = 0; colOffset < 4; colOffset++)
-                    A[(colBase * lda + 4 * row) + colOffset + (stride_a * b)]
-                        = temp[(colBase + colOffset) * lda + row + (stride_a * b)];
-}
-
-template <typename T>
-void hipblas_packInt8(T* A, const T* temp, size_t M, size_t N, size_t lda)
-{
-    if(N % 4 != 0)
-        std::cerr << "ERROR: dimension must be a multiple of 4 in order to pack" << std::endl;
-
-    for(size_t colBase = 0; colBase < N; colBase += 4)
-        for(size_t row = 0; row < lda; row++)
-            for(size_t colOffset = 0; colOffset < 4; colOffset++)
-                A[(colBase * lda + 4 * row) + colOffset] = temp[(colBase + colOffset) * lda + row];
-}
-
-/* ============================================================================================ */
-
-/* ============================================================================================ */
 /*! \brief  matrix/vector initialization: */
 // for vector x (M=1, N=lengthX, lda=incx);
 // for complex number, the real/imag part would be initialized with the same value
