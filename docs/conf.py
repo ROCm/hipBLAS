44521d6d64bc20be12803c7a257ecca39b036605
Sam Wu <samwu103@amd.com>
2023-03-17 13:29:08 -0600

    Revert "add version number in docs"
    This reverts commit 30ae6c38160c5b2cf21c9a55236477ffdd7782a4.

--- docs/conf.py
+++ docs/conf.py
@@ -4,15 +4,9 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import subprocess
 from rocm_docs import ROCmDocs
 
-getVersionCmd = r'sed -n -e "s/^.*VERSION_STRING.* \"\([0-9\.]\{1,\}\).*/\1/p" ../CMakeLists.txt'
-version = subprocess.getoutput(getVersionCmd)
-if len(version) == 0:
-    version = '1.0.0' # optional version number override if CMakeLists.txt does not have the version
-
-docs_core = ROCmDocs("hipBLAS {}".format(version))
+docs_core = ROCmDocs("hipBLAS Documentation")
 docs_core.run_doxygen()
 docs_core.setup()
 
