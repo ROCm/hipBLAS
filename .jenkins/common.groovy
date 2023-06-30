cf4853c79e544eebcc73a1875c5e6466e6c8eeac
Daine McNiven <51674140+daineAMD@users.noreply.github.com>
2023-06-30 12:39:12 -0600

    Deprecating hipblasDatatype_t for hipDataType; adding HIPBLAS_V2 Compile Option (#621)
    [ROCm/hipBLAS commit: a7dae55e02920390b248945b24c523d526e41447]

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -36,6 +36,17 @@ def runTestCommand (platform, project)
                 """
 
     platform.runCommand(this, command)
+
+    // In an upcoming release, we are replacing hipblasDatatype_t with hipDataType. We have created hipblas_v2-test to test the new
+    // interfaces while hipblasDatatype_t is deprecated. Thus, hipblas-test will be testing the old, deprecated, functions
+    // using hipblasDatatype_t, and hipblas_v2-test will be testing the upcoming interfaces.
+    def v2TestCommand = """#!/usr/bin/env bash
+                    set -x
+                    cd ${project.paths.project_build_prefix}/build/release/clients/staging
+                    ${sudo} LD_LIBRARY_PATH=/opt/rocm/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./hipblas_v2-test --gtest_filter=*scal_ex*:*scal_batched_ex*:*scal_strided_batched_ex* --gtest_output=xml --gtest_color=yes
+                """
+
+    platform.runCommand(this, v2TestCommand)
     junit "${project.paths.project_build_prefix}/build/release/clients/staging/*.xml"
 }
 
