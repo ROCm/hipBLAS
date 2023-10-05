7beca7b5a8e9e74506387c8a54ffb15cff1c1b33
Daine McNiven <51674140+daineAMD@users.noreply.github.com>
2023-10-05 09:14:08 -0600

    Yaml testing for all L3 functions (#692)
    [ROCm/hipBLAS commit: 701ed40388ba51a449e90f34135e110fe29131f8]

---
+++ clients/include/blas3/testing_gemm.hpp
@@ -0,0 +1,224 @@
+/* ************************************************************************
+ * Copyright (C) 2016-2023 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * ************************************************************************ */
+
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+#include "testing_common.hpp"
+#include <typeinfo>
+
+/* ============================================================================================ */
+
+using hipblasGemmModel = ArgumentModel<e_a_type,
+                                       e_transA,
+                                       e_transB,
+                                       e_M,
+                                       e_N,
+                                       e_K,
+                                       e_alpha,
+                                       e_lda,
+                                       e_ldb,
+                                       e_beta,
+                                       e_ldc>;
+
+inline void testname_gemm(const Arguments& arg, std::string& name)
+{
+    hipblasGemmModel{}.test_name(arg, name);
+}
+
+template <typename T>
+void testing_gemm(const Arguments& arg)
+{
+    bool FORTRAN       = arg.fortran;
+    auto hipblasGemmFn = FORTRAN ? hipblasGemm<T, true> : hipblasGemm<T, false>;
+
+    hipblasOperation_t transA = char2hipblas_operation(arg.transA);
+    hipblasOperation_t transB = char2hipblas_operation(arg.transB);
+    int                M      = arg.M;
+    int                N      = arg.N;
+    int                K      = arg.K;
+    int                lda    = arg.lda;
+    int                ldb    = arg.ldb;
+    int                ldc    = arg.ldc;
+
+    T h_alpha = arg.get_alpha<T>();
+    T h_beta  = arg.get_beta<T>();
+
+    int A_row, A_col, B_row, B_col;
+
+    if(transA == HIPBLAS_OP_N)
+    {
+        A_row = M;
+        A_col = K;
+    }
+    else
+    {
+        A_row = K;
+        A_col = M;
+    }
+
+    if(transB == HIPBLAS_OP_N)
+    {
+        B_row = K;
+        B_col = N;
+    }
+    else
+    {
+        B_row = N;
+        B_col = K;
+    }
+
+    size_t A_size = size_t(lda) * A_col;
+    size_t B_size = size_t(ldb) * B_col;
+    size_t C_size = size_t(ldc) * N;
+
+    // check here to prevent undefined memory allocation error
+    if(M < 0 || N < 0 || K < 0 || lda < A_row || ldb < B_row || ldc < M)
+    {
+        return;
+    }
+
+    double             gpu_time_used, hipblas_error_host, hipblas_error_device;
+    hipblasLocalHandle handle(arg);
+
+    // Naming: dX is in GPU (device) memory. hK is in CPU (host) memory, plz follow this practice
+    host_vector<T> hA(A_size);
+    host_vector<T> hB(B_size);
+    host_vector<T> hC_host(C_size);
+    host_vector<T> hC_device(C_size);
+    host_vector<T> hC_copy(C_size);
+
+    device_vector<T> dA(A_size);
+    device_vector<T> dB(B_size);
+    device_vector<T> dC(C_size);
+    device_vector<T> d_alpha(1);
+    device_vector<T> d_beta(1);
+
+    // Initial Data on CPU
+    hipblas_init_matrix(hA, arg, A_row, A_col, lda, 0, 1, hipblas_client_alpha_sets_nan, true);
+    hipblas_init_matrix(
+        hB, arg, B_row, B_col, ldb, 0, 1, hipblas_client_alpha_sets_nan, false, true);
+    hipblas_init_matrix(hC_host, arg, M, N, ldc, 0, 1, hipblas_client_beta_sets_nan);
+
+    // copy vector is easy in STL; hz = hx: save a copy in hC_copy which will be output of CPU BLAS
+    hC_copy   = hC_host;
+    hC_device = hC_host;
+
+    // copy data from CPU to device, does not work for lda != A_row
+    ASSERT_HIP_SUCCESS(hipMemcpy(dA, hA, sizeof(T) * lda * A_col, hipMemcpyHostToDevice));
+    ASSERT_HIP_SUCCESS(hipMemcpy(dB, hB, sizeof(T) * ldb * B_col, hipMemcpyHostToDevice));
+    ASSERT_HIP_SUCCESS(hipMemcpy(dC, hC_host, sizeof(T) * ldc * N, hipMemcpyHostToDevice));
+    ASSERT_HIP_SUCCESS(hipMemcpy(d_alpha, &h_alpha, sizeof(T), hipMemcpyHostToDevice));
+    ASSERT_HIP_SUCCESS(hipMemcpy(d_beta, &h_beta, sizeof(T), hipMemcpyHostToDevice));
+
+    if(arg.unit_check || arg.norm_check)
+    {
+        /* =====================================================================
+            HIPBLAS
+        =================================================================== */
+        ASSERT_HIPBLAS_SUCCESS(hipblasSetPointerMode(handle, HIPBLAS_POINTER_MODE_HOST));
+
+        // library interface
+        ASSERT_HIPBLAS_SUCCESS(hipblasGemmFn(
+            handle, transA, transB, M, N, K, &h_alpha, dA, lda, dB, ldb, &h_beta, dC, ldc));
+
+        // copy output from device to CPU
+        ASSERT_HIP_SUCCESS(hipMemcpy(hC_host, dC, sizeof(T) * ldc * N, hipMemcpyDeviceToHost));
+
+        ASSERT_HIP_SUCCESS(hipMemcpy(dC, hC_device, sizeof(T) * ldc * N, hipMemcpyHostToDevice));
+        ASSERT_HIPBLAS_SUCCESS(hipblasSetPointerMode(handle, HIPBLAS_POINTER_MODE_DEVICE));
+        ASSERT_HIPBLAS_SUCCESS(hipblasGemmFn(
+            handle, transA, transB, M, N, K, d_alpha, dA, lda, dB, ldb, d_beta, dC, ldc));
+        ASSERT_HIP_SUCCESS(hipMemcpy(hC_device, dC, sizeof(T) * ldc * N, hipMemcpyDeviceToHost));
+
+        /* =====================================================================
+                    CPU BLAS
+        =================================================================== */
+        cblas_gemm<T>(transA,
+                      transB,
+                      M,
+                      N,
+                      K,
+                      h_alpha,
+                      hA.data(),
+                      lda,
+                      hB.data(),
+                      ldb,
+                      h_beta,
+                      hC_copy.data(),
+                      ldc);
+
+        // enable unit check, notice unit check is not invasive, but norm check is,
+        // unit check and norm check can not be interchanged their order
+        if(arg.unit_check)
+        {
+            unit_check_general<T>(M, N, ldc, hC_copy, hC_host);
+            unit_check_general<T>(M, N, ldc, hC_copy, hC_device);
+        }
+        if(arg.norm_check)
+        {
+            hipblas_error_host = std::abs(norm_check_general<T>('F', M, N, ldc, hC_copy, hC_host));
+            hipblas_error_device
+                = std::abs(norm_check_general<T>('F', M, N, ldc, hC_copy, hC_device));
+        }
+
+    } // end of if unit/norm check
+
+    if(arg.timing)
+    {
+        hipStream_t stream;
+        ASSERT_HIPBLAS_SUCCESS(hipblasGetStream(handle, &stream));
+
+        // gemm has better performance in host mode. In rocBLAS in device mode
+        // we need to copy alpha and beta to the host.
+        ASSERT_HIPBLAS_SUCCESS(hipblasSetPointerMode(handle, HIPBLAS_POINTER_MODE_HOST));
+
+        int runs = arg.cold_iters + arg.iters;
+        for(int iter = 0; iter < runs; iter++)
+        {
+            if(iter == arg.cold_iters)
+                gpu_time_used = get_time_us_sync(stream);
+
+            ASSERT_HIPBLAS_SUCCESS(hipblasGemmFn(
+                handle, transA, transB, M, N, K, &h_alpha, dA, lda, dB, ldb, &h_beta, dC, ldc));
+        }
+        gpu_time_used = get_time_us_sync(stream) - gpu_time_used;
+
+        hipblasGemmModel{}.log_args<T>(std::cout,
+                                       arg,
+                                       gpu_time_used,
+                                       gemm_gflop_count<T>(M, N, K),
+                                       gemm_gbyte_count<T>(M, N, K),
+                                       hipblas_error_host,
+                                       hipblas_error_device);
+    }
+}
+
+template <typename T>
+hipblasStatus_t testing_gemm_ret(const Arguments& arg)
+{
+    testing_gemm<T>(arg);
+    return HIPBLAS_STATUS_SUCCESS;
+}
---
+++ clients/include/blas3/testing_trsm.hpp
@@ -0,0 +1,199 @@
+/* ************************************************************************
+ * Copyright (C) 2016-2023 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * ************************************************************************ */
+
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+#include "testing_common.hpp"
+
+/* ============================================================================================ */
+
+using hipblasTrsmModel
+    = ArgumentModel<e_a_type, e_side, e_uplo, e_transA, e_diag, e_M, e_N, e_alpha, e_lda, e_ldb>;
+
+inline void testname_trsm(const Arguments& arg, std::string& name)
+{
+    hipblasTrsmModel{}.test_name(arg, name);
+}
+
+template <typename T>
+void testing_trsm(const Arguments& arg)
+{
+    bool FORTRAN       = arg.fortran;
+    auto hipblasTrsmFn = FORTRAN ? hipblasTrsm<T, true> : hipblasTrsm<T, false>;
+
+    hipblasSideMode_t  side   = char2hipblas_side(arg.side);
+    hipblasFillMode_t  uplo   = char2hipblas_fill(arg.uplo);
+    hipblasOperation_t transA = char2hipblas_operation(arg.transA);
+    hipblasDiagType_t  diag   = char2hipblas_diagonal(arg.diag);
+    int                M      = arg.M;
+    int                N      = arg.N;
+    int                lda    = arg.lda;
+    int                ldb    = arg.ldb;
+
+    T h_alpha = arg.get_alpha<T>();
+
+    int    K      = (side == HIPBLAS_SIDE_LEFT ? M : N);
+    size_t A_size = size_t(lda) * K;
+    size_t B_size = size_t(ldb) * N;
+
+    // check here to prevent undefined memory allocation error
+    if(M < 0 || N < 0 || lda < K || ldb < M)
+    {
+        return;
+    }
+    // Naming: dK is in GPU (device) memory. hK is in CPU (host) memory
+    host_vector<T> hA(A_size);
+    host_vector<T> hB_host(B_size);
+    host_vector<T> hB_device(B_size);
+    host_vector<T> hB_gold(B_size);
+
+    device_vector<T> dA(A_size);
+    device_vector<T> dB(B_size);
+    device_vector<T> d_alpha(1);
+
+    double             gpu_time_used, hipblas_error_host, hipblas_error_device;
+    hipblasLocalHandle handle(arg);
+
+    // Initial hA on CPU
+    hipblas_init_matrix(hA, arg, K, K, lda, 0, 1, hipblas_client_never_set_nan, true);
+    hipblas_init_matrix(hB_host, arg, M, N, ldb, 0, 1, hipblas_client_never_set_nan);
+
+    // pad untouched area into zero
+    for(int i = K; i < lda; i++)
+    {
+        for(int j = 0; j < K; j++)
+        {
+            hA[i + j * lda] = 0.0;
+        }
+    }
+    // proprocess the matrix to avoid ill-conditioned matrix
+    std::vector<int> ipiv(K);
+    cblas_getrf(K, K, hA.data(), lda, ipiv.data());
+    for(int i = 0; i < K; i++)
+    {
+        for(int j = i; j < K; j++)
+        {
+            hA[i + j * lda] = hA[j + i * lda];
+            if(diag == HIPBLAS_DIAG_UNIT)
+            {
+                if(i == j)
+                    hA[i + j * lda] = 1.0;
+            }
+        }
+    }
+
+    // pad untouched area into zero
+    for(int i = M; i < ldb; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            hB_host[i + j * ldb] = 0.0;
+        }
+    }
+    hB_gold = hB_host; // original solution hX
+
+    // Calculate hB = hA*hX;
+    cblas_trmm<T>(
+        side, uplo, transA, diag, M, N, T(1.0) / h_alpha, (const T*)hA, lda, hB_host, ldb);
+
+    hB_device = hB_host;
+
+    // copy data from CPU to device
+    ASSERT_HIP_SUCCESS(hipMemcpy(dA, hA, sizeof(T) * A_size, hipMemcpyHostToDevice));
+    ASSERT_HIP_SUCCESS(hipMemcpy(dB, hB_host, sizeof(T) * B_size, hipMemcpyHostToDevice));
+    ASSERT_HIP_SUCCESS(hipMemcpy(d_alpha, &h_alpha, sizeof(T), hipMemcpyHostToDevice));
+
+    /* =====================================================================
+           HIPBLAS
+    =================================================================== */
+    if(arg.unit_check || arg.norm_check)
+    {
+        ASSERT_HIPBLAS_SUCCESS(hipblasSetPointerMode(handle, HIPBLAS_POINTER_MODE_HOST));
+        ASSERT_HIPBLAS_SUCCESS(
+            hipblasTrsmFn(handle, side, uplo, transA, diag, M, N, &h_alpha, dA, lda, dB, ldb));
+
+        ASSERT_HIP_SUCCESS(hipMemcpy(hB_host, dB, sizeof(T) * B_size, hipMemcpyDeviceToHost));
+        ASSERT_HIP_SUCCESS(hipMemcpy(dB, hB_device, sizeof(T) * B_size, hipMemcpyHostToDevice));
+
+        ASSERT_HIPBLAS_SUCCESS(hipblasSetPointerMode(handle, HIPBLAS_POINTER_MODE_DEVICE));
+        ASSERT_HIPBLAS_SUCCESS(
+            hipblasTrsmFn(handle, side, uplo, transA, diag, M, N, d_alpha, dA, lda, dB, ldb));
+
+        ASSERT_HIP_SUCCESS(hipMemcpy(hB_device, dB, sizeof(T) * B_size, hipMemcpyDeviceToHost));
+
+        /* =====================================================================
+           CPU BLAS
+        =================================================================== */
+
+        // cblas_trsm<T>(
+        //     side, uplo, transA, diag, M, N, h_alpha, (const T*)hA, lda, hB_gold, ldb);
+
+        // if enable norm check, norm check is invasive
+        real_t<T> eps       = std::numeric_limits<real_t<T>>::epsilon();
+        double    tolerance = eps * 40 * M;
+
+        hipblas_error_host   = norm_check_general<T>('F', M, N, ldb, hB_gold, hB_host);
+        hipblas_error_device = norm_check_general<T>('F', M, N, ldb, hB_gold, hB_device);
+        if(arg.unit_check)
+        {
+            unit_check_error(hipblas_error_host, tolerance);
+            unit_check_error(hipblas_error_device, tolerance);
+        }
+    }
+
+    if(arg.timing)
+    {
+        hipStream_t stream;
+        ASSERT_HIPBLAS_SUCCESS(hipblasGetStream(handle, &stream));
+        ASSERT_HIPBLAS_SUCCESS(hipblasSetPointerMode(handle, HIPBLAS_POINTER_MODE_DEVICE));
+
+        int runs = arg.cold_iters + arg.iters;
+        for(int iter = 0; iter < runs; iter++)
+        {
+            if(iter == arg.cold_iters)
+                gpu_time_used = get_time_us_sync(stream);
+
+            ASSERT_HIPBLAS_SUCCESS(
+                hipblasTrsmFn(handle, side, uplo, transA, diag, M, N, d_alpha, dA, lda, dB, ldb));
+        }
+        gpu_time_used = get_time_us_sync(stream) - gpu_time_used;
+
+        hipblasTrsmModel{}.log_args<T>(std::cout,
+                                       arg,
+                                       gpu_time_used,
+                                       trsm_gflop_count<T>(M, N, K),
+                                       trsm_gbyte_count<T>(M, N, K),
+                                       hipblas_error_host,
+                                       hipblas_error_device);
+    }
+}
+
+template <typename T>
+hipblasStatus_t testing_trsm_ret(const Arguments& arg)
+{
+    testing_trsm<T>(arg);
+    return HIPBLAS_STATUS_SUCCESS;
+}
