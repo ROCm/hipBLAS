1d52e3e30408034396e15caefce844a5cfcf6881
Daine McNiven <51674140+daineAMD@users.noreply.github.com>
2023-12-08 08:53:06 -0700

    Enabling and adding bad-arg tests for rocsolver (#733)

--- clients/include/solver/testing_getri_batched.hpp
+++ clients/include/solver/testing_getri_batched.hpp
@@ -36,6 +36,102 @@ inline void testname_getri_batched(const Arguments& arg, std::string& name)
 }
 
 template <typename T>
+void testing_getri_batched_bad_arg(const Arguments& arg)
+{
+    bool FORTRAN = arg.fortran;
+    auto hipblasGetriBatchedFn
+        = FORTRAN ? hipblasGetriBatched<T, true> : hipblasGetriBatched<T, false>;
+
+    hipblasLocalHandle handle(arg);
+    int64_t            N           = 101;
+    int64_t            M           = N;
+    int64_t            lda         = 102;
+    int64_t            batch_count = 2;
+    int64_t            A_size      = N * lda;
+    int64_t            Ipiv_size   = std::min(M, N) * batch_count;
+
+    device_batch_vector<T> dA(A_size, 1, batch_count);
+    device_batch_vector<T> dC(A_size, 1, batch_count);
+    device_vector<int>     dIpiv(Ipiv_size);
+    device_vector<int>     dInfo(batch_count);
+
+    EXPECT_HIPBLAS_STATUS(hipblasGetriBatchedFn(nullptr,
+                                                N,
+                                                dA.ptr_on_device(),
+                                                lda,
+                                                dIpiv,
+                                                dC.ptr_on_device(),
+                                                lda,
+                                                dInfo,
+                                                batch_count),
+                          HIPBLAS_STATUS_NOT_INITIALIZED);
+
+    EXPECT_HIPBLAS_STATUS(hipblasGetriBatchedFn(handle,
+                                                -1,
+                                                dA.ptr_on_device(),
+                                                lda,
+                                                dIpiv,
+                                                dC.ptr_on_device(),
+                                                lda,
+                                                dInfo,
+                                                batch_count),
+                          HIPBLAS_STATUS_INVALID_VALUE);
+
+    EXPECT_HIPBLAS_STATUS(hipblasGetriBatchedFn(handle,
+                                                N,
+                                                dA.ptr_on_device(),
+                                                N - 1,
+                                                dIpiv,
+                                                dC.ptr_on_device(),
+                                                lda,
+                                                dInfo,
+                                                batch_count),
+                          HIPBLAS_STATUS_INVALID_VALUE);
+
+    EXPECT_HIPBLAS_STATUS(hipblasGetriBatchedFn(handle,
+                                                N,
+                                                dA.ptr_on_device(),
+                                                lda,
+                                                dIpiv,
+                                                dC.ptr_on_device(),
+                                                N - 1,
+                                                dInfo,
+                                                batch_count),
+                          HIPBLAS_STATUS_INVALID_VALUE);
+
+    EXPECT_HIPBLAS_STATUS(
+        hipblasGetriBatchedFn(
+            handle, N, dA.ptr_on_device(), lda, dIpiv, dC.ptr_on_device(), lda, dInfo, -1),
+        HIPBLAS_STATUS_INVALID_VALUE);
+
+    // If N == 0 || batch_count == 0, A, C, and ipiv can be nullptr. rocSolver doesn't allow nullptr with batch_count == 0
+    CHECK_HIPBLAS_ERROR(
+        hipblasGetriBatchedFn(handle, 0, nullptr, lda, nullptr, nullptr, lda, dInfo, batch_count));
+
+    if(arg.bad_arg_all)
+    {
+        EXPECT_HIPBLAS_STATUS(
+            hipblasGetriBatchedFn(
+                handle, N, nullptr, lda, dIpiv, dC.ptr_on_device(), lda, dInfo, batch_count),
+            HIPBLAS_STATUS_INVALID_VALUE);
+        EXPECT_HIPBLAS_STATUS(
+            hipblasGetriBatchedFn(
+                handle, N, dA.ptr_on_device(), lda, dIpiv, nullptr, lda, dInfo, batch_count),
+            HIPBLAS_STATUS_INVALID_VALUE);
+        EXPECT_HIPBLAS_STATUS(hipblasGetriBatchedFn(handle,
+                                                    N,
+                                                    dA.ptr_on_device(),
+                                                    lda,
+                                                    dIpiv,
+                                                    dC.ptr_on_device(),
+                                                    lda,
+                                                    nullptr,
+                                                    batch_count),
+                              HIPBLAS_STATUS_INVALID_VALUE);
+    }
+}
+
+template <typename T>
 void testing_getri_batched(const Arguments& arg)
 {
     using U      = real_t<T>;
