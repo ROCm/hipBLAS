2c32d96c3102a5a551750c4f08c0abd86068bcb3
daineAMD <daine.mcniven@amd.com>
2023-12-18 17:14:45 -0700

    Adding tests back for amd backend.

--- clients/gtest/blas2/gemv_gtest.cpp
+++ clients/gtest/blas2/gemv_gtest.cpp
@@ -50,7 +50,7 @@ namespace
                 if(!hipblas_client_global_filters(args))
                     return false;
 
-#if CUBLAS_VERSION < 110700
+#if defined(__HIP_PLATFORM_NVCC__) && CUBLAS_VERSION < 110700
                 // avoid gemvBatched/gemvStridedBatched tests with cuBLAS older than 11.7.0
                 if(!strcmp(args.function, "gemv_batched")
                    || !strcmp(args.function, "gemv_batched_bad_arg")
@@ -111,10 +111,12 @@ namespace
     // When the condition in the second argument is satisfied, the type combination
     // is valid. When the condition is false, this specialization does not apply.
     template <typename T>
-    struct gemv_testing<T,
-                        std::enable_if_t<std::is_same_v<T, float> || std::is_same_v<T, double>
-                                         || std::is_same_v<T, hipblasComplex>
-                                         || std::is_same_v<T, hipblasDoubleComplex>>>
+    struct gemv_testing<
+        T,
+        std::enable_if_t<
+            std::is_same_v<
+                T,
+                float> || std::is_same_v<T, double> || std::is_same_v<T, hipblasComplex> || std::is_same_v<T, hipblasDoubleComplex>>>
         : hipblas_test_valid
     {
         void operator()(const Arguments& arg)
