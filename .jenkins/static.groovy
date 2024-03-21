e9263549e096bcee4d5a772102a29224140a1a7d
Sam Wu <sam.wu2@amd.com>
2024-03-21 10:28:08 -0600

    Remove deprecated option from static job build command
    install.sh has no -p option to specify cmake path

--- .jenkins/static.groovy
+++ .jenkins/static.groovy
@@ -14,7 +14,7 @@ def runCI =
     nodeDetails, jobName->
 
     def prj  = new rocProject('hipBLAS', 'static')
-    prj.paths.build_command = './install.sh -cd --static -p /opt/rocm/lib/cmake'
+    prj.paths.build_command = './install.sh -cd --static'
 
     if (env.BRANCH_NAME ==~ /PR-\d+/ && pullRequest.labels.contains("noSolver"))
     {
