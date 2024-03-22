d2b485c177d0cc49fe3d72a2875ec2102f154fca
Sam Wu <sam.wu2@amd.com>
2024-03-22 14:30:06 -0600

    Use --static flag instead of -s for static lib ci

--- .jenkins/static.groovy
+++ .jenkins/static.groovy
@@ -14,7 +14,7 @@ def runCI =
     nodeDetails, jobName->
 
     def prj  = new rocProject('hipBLAS', 'static')
-    prj.paths.build_command = './install.sh -cs'
+    prj.paths.build_command = './install.sh -c --static'
 
     if (env.BRANCH_NAME ==~ /PR-\d+/ && pullRequest.labels.contains("noSolver"))
     {
