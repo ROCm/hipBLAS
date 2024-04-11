c678e0751502f43859f715465194f884ba7ac9de
Sam Wu <sam.wu2@amd.com>
2024-04-11 17:13:02 -0600

    Re-enable static library job in CI (#795)
    * Re-enable static library job in CI
    
    * Remove deprecated option from static job build command
    
    install.sh has no -p option to specify cmake path
    
    * Add rocPRIM for static lib ci
    
    * Use -s option instead of --static
    
    * Add -s flag for static in rmake
    
    * Skip building deps for static ci
    
    * Use --static flag instead of -s for static lib ci
    
    * Use -s flag for static ci
    
    * Use hipcc as compiler
    
    * Update static.groovy
    
    Remove rocPRIM as dep when noSolver label is applied
    
    * Put rocPRIM as dep for static job
    
    * Remove rocPRIM as dep when not using rocSOLVER
    
    [ROCm/hipBLAS commit: e5322f9a4c1c3e68f44766c330ac0ba6ff23f163]

--- .jenkins/static.groovy
+++ .jenkins/static.groovy
@@ -13,8 +13,8 @@ def runCI =
 {
     nodeDetails, jobName->
 
-    def prj  = new rocProject('hipBLAS', 'StaticLibrary')
-    prj.paths.build_command = './install.sh -cd --static -p /opt/rocm/lib/cmake'
+    def prj  = new rocProject('hipBLAS', 'static')
+    prj.paths.build_command = './install.sh -cs --compiler=hipcc'
 
     if (env.BRANCH_NAME ==~ /PR-\d+/ && pullRequest.labels.contains("noSolver"))
     {
@@ -22,7 +22,7 @@ def runCI =
     }
     else
     {
-        prj.libraryDependencies = ['rocBLAS', 'rocSPARSE', 'rocSOLVER']
+        prj.libraryDependencies = ['rocBLAS', 'rocSPARSE', 'rocSOLVER', 'rocPRIM']
     }
 
     // Define test architectures, optional rocm version argument is available
@@ -87,7 +87,7 @@ ci: {
     {
         properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
         stage(urlJobName) {
-            runCI([ubuntu18:['gfx900']], urlJobName)
+            runCI([ubuntu18:['gfx906']], urlJobName)
         }
     }
 }
