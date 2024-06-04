7d61844367371a4dca1d1a01db0fa4a8a8cd160d
NaveenElumalaiAMD <nelumala@amd.com>
2024-06-04 15:58:16 -0500

    Fix rotm_strided_batched failures

--- clients/include/blas1/testing_rotm_batched.hpp
+++ clients/include/blas1/testing_rotm_batched.hpp
@@ -128,6 +128,12 @@ void testing_rotm_batched(const Arguments& arg)
 
     for(int64_t b = 0; b < batch_count; b++)
     {
+        T* hparam_ptr = hparam[b];
+
+        // generating simply one set of hparam which will not be appropriate for testing
+        // that it zeros out the second element of the rotm vector parameter
+        memset(hparam_ptr, 0, 5 * sizeof(T));
+
         ref_rotmg<T>(&hdata[b][0], &hdata[b][1], &hdata[b][2], &hdata[b][3], hparam[b]);
     }
 
