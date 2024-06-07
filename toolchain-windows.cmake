950735b5071c2526246238c6436d2163f935d004
Steve Leung <Steve.Leung@amd.com>
2024-06-07 10:11:57 -0600

    cmake: don't set CMAKE_C_COMPILER, as hipBLAS is a CXX project (#877)
    [ROCm/hipBLAS commit: aa9247cc19e60d13a014e4ff0899814ab8a1511e]

--- toolchain-windows.cmake
+++ toolchain-windows.cmake
@@ -11,7 +11,6 @@ else()
 endif()
 
 set(CMAKE_CXX_COMPILER "${rocm_bin}/clang++.exe")
-set(CMAKE_C_COMPILER "${rocm_bin}/clang.exe")
  
 if (NOT python)
   set(python "python") # take default for windows
@@ -23,7 +22,6 @@ endif()
 #set(CMAKE_Fortran_PREPROCESS_SOURCE "<CMAKE_Fortran_COMPILER> -E <INCLUDES> <FLAGS> -cpp <SOURCE> -o <PREPROCESSED_SOURCE>")
 
 # TODO remove, just to speed up slow cmake
-#set(CMAKE_C_COMPILER_WORKS 1)
 #set(CMAKE_CXX_COMPILER_WORKS 1)
 #set(CMAKE_Fortran_COMPILER_WORKS 1)
 
