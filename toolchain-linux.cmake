5dfc5a091feb03ab05937907f4eceb75ce68d32e
Daine McNiven <51674140+daineAMD@users.noreply.github.com>
2024-06-18 10:26:29 -0600

    Change default compiler to amdclang++ (#881)
    [ROCm/hipBLAS commit: f4771ca42819a80610a874bd2544bf1a0f7a16ad]

--- toolchain-linux.cmake
+++ toolchain-linux.cmake
@@ -11,15 +11,12 @@ endif()
 
 # relying on env and path for backward compatibility with external recipes
 if (NOT DEFINED ENV{CXX} AND NOT CMAKE_CXX_COMPILER)
-  set(CMAKE_CXX_COMPILER "${rocm_bin}/hipcc")
+  set(CMAKE_CXX_COMPILER "${rocm_bin}/amdclang++")
 endif()
 
 if (NOT DEFINED ENV{FC} AND NOT CMAKE_Fortran_COMPILER)
   set(CMAKE_Fortran_COMPILER "gfortran")
 endif()
-
-# relying on path for backward compatibility
-# set(CMAKE_CXX_COMPILER "${rocm_bin}/hipcc")
 
 if (CONFIG_NO_COMPILER_CHECKS)
   set(CMAKE_CXX_COMPILER_WORKS 1)
