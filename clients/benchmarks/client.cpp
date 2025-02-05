812b459fdf433577aee807e9932adc9d183e47e6
daineAMD <daine.mcniven@amd.com>
2025-02-04 17:51:05 -0700

    Add user_allocated_workspace as argument for clients; test setWorkspace in trsm tests.
    [ROCm/hipBLAS commit: 9edf11656f98620bc53d231fbd36034a3954e832]

--- clients/benchmarks/client.cpp
+++ clients/benchmarks/client.cpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2016-2023 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2016-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -368,6 +368,10 @@ try
         ("api",
          value<int32_t>(&api)->default_value(0),
          "Use API, supercedes fortran flag (0==C, 1==C_64, ...)")
+
+        ("workspace",
+         value<size_t>(&arg.user_allocated_workspace)->default_value(0),
+         "Set workspace available in handle using xxblasSetWorkspace() API after handle creation"),
 
         ("help,h", "produces this help message");
 
--- clients/include/blas3/testing_trsm.hpp
+++ clients/include/blas3/testing_trsm.hpp
@@ -1,5 +1,5 @@
 /* ************************************************************************
- * Copyright (C) 2016-2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2016-2025 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -30,8 +30,17 @@
 
 /* ============================================================================================ */
 
-using hipblasTrsmModel
-    = ArgumentModel<e_a_type, e_side, e_uplo, e_transA, e_diag, e_M, e_N, e_alpha, e_lda, e_ldb>;
+using hipblasTrsmModel = ArgumentModel<e_a_type,
+                                       e_side,
+                                       e_uplo,
+                                       e_transA,
+                                       e_diag,
+                                       e_M,
+                                       e_N,
+                                       e_alpha,
+                                       e_lda,
+                                       e_ldb,
+                                       e_user_allocated_workspace>;
 
 inline void testname_trsm(const Arguments& arg, std::string& name)
 {
