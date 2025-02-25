3cbdde71e9c9b3ed182e0c38df8778b710c95b44
amd-garydeng <garydeng@amd.com>
2025-02-25 13:48:20 -0700

    Add ROCM-Examples

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -38,7 +38,7 @@ def runCompileCommand(platform, project, jobName, boolean sameOrg=false)
     platform.runCommand(this, command)
 }
 
-def runTestCommand (platform, project)
+def runTestCommand (platform, project, boolean rocmExamples)
 {
     String sudo = auxiliary.sudo(platform.jenkinsLabel)
     String stagingDir = "${project.paths.project_build_prefix}/build/release/clients/staging"
@@ -78,6 +78,34 @@ def runTestCommand (platform, project)
                 """
     platform.runCommand(this, yamlTestCommand)
     junit "${stagingDir}/*.xml"
+
+    //ROCM-Examples
+    if (rocmExamples){
+        String buildString = ""
+        if (platform.os.contains("ubuntu")){
+            buildString += "sudo dpkg -i *.deb"
+        }
+        else {
+            buildString += "sudo rpm -i *.rpm"
+        }
+        testCommand = """#!/usr/bin/env bash
+                    set -ex
+                    cd ${project.paths.project_build_prefix}/build/release/package
+                    ${buildString}
+                    cd ../../..
+                    testDirs=("Libraries/hipBLAS")
+                    git clone https://github.com/ROCm/rocm-examples.git
+                    rocm_examples_dir=\$(readlink -f rocm-examples)
+                    for testDir in \${testDirs[@]}; do
+                        cd \${rocm_examples_dir}/\${testDir}
+                        cmake -S . -B build
+                        cmake --build build
+                        cd ./build
+                        ctest --output-on-failure
+                    done
+                """
+        platform.runCommand(this, testCommand, "ROCM Examples")  
+    }
 }
 
 def runPackageCommand(platform, project, jobName, label='')
--- .jenkins/precheckin.groovy
+++ .jenkins/precheckin.groovy
@@ -59,7 +59,7 @@ def runCI =
     {
         platform, project->
 
-        commonGroovy.runTestCommand(platform, project)
+        commonGroovy.runTestCommand(platform, project, true)
     }
 
     def packageCommand =
