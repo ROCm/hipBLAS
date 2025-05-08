9910691e4bcc730825fceb08c7bed9f11718c9fe
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2025-05-08 09:59:47 -0600

    minimalistic deprecated API removal (#1009)
    * remove use of rocblas_set_device_memory_size which is deprecated
    
    [ROCm/hipBLAS commit: ac440162d9d9d807f00c2cc5244370a003f2b712]

--- library/src/amd_detail/hipblas.cpp
+++ library/src/amd_detail/hipblas.cpp
@@ -57,11 +57,7 @@ static hipblasStatus_t hipblasDemandAlloc(rocblas_handle                   handl
                     status = hipblasConvertStatus(blas_status);
                 else
                 {
-                    blas_status = rocblas_set_device_memory_size(handle, size);
-                    if(blas_status != rocblas_status_success)
-                        status = hipblasConvertStatus(blas_status);
-                    else
-                        status = func();
+                    status = func();
                 }
             }
         }
